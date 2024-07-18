23fdc3b3ae0a6a7323b5cbb0fdfa38b9ba4d2146
SubhamKumar101201 <das.subham101201@gmail.com>
2024-07-18 08:52:41 +0530

    Add day1 and day2 challenge

---
+++ Day1_Varibale_And_Datatypes/Task.js
@@ -0,0 +1,44 @@
+var res = 1
+console.log(res)
+
+let res2 = 23
+console.log(res2);
+
+const value = 42
+console.log(value);
+
+const a = 1, b = "ss", c = true, d = [18,23,52,123]
+
+console.log(typeof a, typeof b, typeof c, typeof d);
+
+
+let val = 12
+
+console.log(val);
+
+val = 34
+
+console.log(val);
+
+const val1 = 12
+
+console.log(val);
+
+val1 = 34
+
+console.log(val);
+
+var name ='subham'
+
+var name = 'kumar'
+
+console.log(name); // kumar returns run perfectly
+
+
+let name1 = 'subham' // in let we cant re decalre variable but reassign
+
+const name2 = 'subham' // in const we can't redeclare and reassign
+
+subham = "sss"
+
+console.log(subham)
---
+++ Day2_Operators/Task.js
@@ -0,0 +1,87 @@
+// Operators
+
+// Activity 1 : Arithmetic Operations
+
+// Task1: 
+let a = 10, b = 21
+
+console.log("Addition =",a+b)
+
+// Task2: 
+
+console.log("Subtraction =",b-a)
+
+// Task3 
+
+console.log("Multiplication =",a*b)
+
+// Task4: 
+
+console.log("Division =",a/b)
+
+// Task5: 
+
+console.log("Remainder =",a%b)
+
+// Activity 2 : Assignment Operator
+
+// Task6:
+
+a+=2
+console.log("Addition Operator =",a)
+
+// Task7:
+
+a-=4
+console.log("Subtraction Operator =",a)
+
+// Activity 3 : Comparison Operator
+
+// Task8:
+
+console.log("Greater than =",a>b)
+console.log("Smaller than =",a<b)
+
+// Task9:
+
+console.log("Greater than equal to =", a>=b);
+console.log("Smaller than equal to =", a<=b);
+
+// Task10:
+
+let c = '10'
+
+console.log("Value Comparison ",10==c)
+
+console.log("Value and Type Comparison ",10===c)
+
+// Activity 4 : Logical Operators
+
+// Task11:
+
+a = 10
+
+console.log("And Operator ", a === 10 && b === 21 );
+
+console.log("And Operator ", a === 10 && b === 20 );
+
+// Task12:
+
+console.log("OR Operator ", a === 11 || b === 21 );
+
+console.log("OR Operator ", a === 11 || b === 20 );
+
+// Task13:
+
+console.log("Not Operator ", !(a===b));
+
+// Activity 5 : Tenary Operator
+
+// Task14:
+
+b = -21
+
+console.log("Ternary Operator ", a >= 0 ? true : false);
+
+console.log("Ternary Operator ", b >= 0 ? true : false);
+
